f6ac403a83c89d1cd2e773bd873c48dd0c605c7e
IoIxD <alphaproject217@gmail.com>
2022-09-27 19:24:41 -0700

    switched the gradient library to one that doens't use floats

--- src/modules/gradient.rs
+++ src/modules/gradient.rs
@@ -1,53 +1,46 @@
 use image::{ImageBuffer, RgbaImage};
 use rand::Rng;
-use colorgrad::Color;
+use tiny_gradient::{gradient::Gradient, RGB};
 
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 768;
+const BLACK_COLOR: RGB = RGB::new(0,0,0);
 
-fn new() -> colorgrad::Gradient {
-    let color_num = rand::thread_rng().gen_range(0..32768);
-    let mut colors = Vec::new();
-    for _ in 1..color_num {
-        let r = rand::thread_rng().gen_range(0..255);
+fn new() -> Gradient {
+    let mut colors: [RGB<u8>; 2] = [BLACK_COLOR,BLACK_COLOR];
+    for i in 0..=1 {
+        let r: u8 = rand::thread_rng().gen_range(0..255);
         let g = rand::thread_rng().gen_range(0..255);
         let b = rand::thread_rng().gen_range(0..255);
 
-        let color = Color::from_rgba8(r,g,b,255);
-
-        colors.push(color)
+        let color = RGB::new(r,g,b);
+        colors[i] = color;
     }
-
-    let g = 
-        match colorgrad::CustomGradient::new()
-        .colors(colors.as_mut_slice())
-        .build() {
-            Ok(g) => g,
-            Err(err) => {
-                panic!("{err}");
-                
-            }
-        };
-    g
+    println!("generated gradient");
+    Gradient::new(colors[0],colors[1],(WIDTH*HEIGHT) as usize)
+    
 }
 
 pub fn new_image() -> RgbaImage {
-    let grad = new();
-    let img: RgbaImage = ImageBuffer::from_fn(WIDTH,HEIGHT, |x, y| {
-        let color = grad.at((x) as f64 / (WIDTH) as f64);
+    let mut grad = new().into_iter();
 
-        println!("\r{}, {}: ({}, {}, {})",
+    let img: RgbaImage = ImageBuffer::from_fn(WIDTH,HEIGHT, 
+                                            |x, y| {
+
+        let color_ = grad.next();
+        if color_ == None {
+            return image::Rgba([0, 0, 0, 255]);
+        }
+        let color = color_.unwrap();
+
+        print!("\r\t\t{:5}, {:5}:\t\t({:3}, {:3}, {:3})",
         x, y,
-        color.r*255.0,
-        color.g*255.0,
-        color.b*255.0);
+        color.r,
+        color.g,
+        color.b);
 
-        image::Rgba([
-            (color.r * 255.0) as u8, 
-            (color.g * 255.0) as u8, 
-            (color.b * 255.0) as u8,
-            255
-        ])
+        image::Rgba([color.r,color.g,color.b,255])
     });
+    println!("\nsaved gradient");
     img
 }
