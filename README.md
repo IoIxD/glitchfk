a9f01c27fbf95cd728f62a3431b5627c0e6803ea
IoIxD <alphaproject217@gmail.com>
2022-10-21 11:52:50 -0700

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Glitchfuck
+
+This is the repository for code that generates random gradients and noise and xor's them together. Unlike [IoIxD/glitchfuckOld](https://github.com/IoIxD/glitchfuckOld/), this is a more complete ~~CLI~~ program ~~that contains code for a server that autoamtically posts these to Twitter, as well as a Discord bot that lets you generate them via slash commands.~~ Currently finishing up a rewrite, these will be added in due time.
+
+![Fa0Loc0WIAA42U8](https://user-images.githubusercontent.com/30945097/186069771-58d66f9f-2d08-478b-83f5-825271d5532e.jpeg)
--- src/image.rs
+++ src/image.rs
@@ -3,26 +3,21 @@ use std::{io::BufWriter, borrow::BorrowMut};
 use image::{RgbImage, Rgb};
 
 pub fn xor_images(img1: RgbImage, img2: RgbImage) -> RgbImage {
-    // todo: unravel both images arrays because those are faster
-    // to work with. 
-
-    let mut img1_iter = img1.pixels().into_iter();
-    let mut img2_iter = img2.pixels().into_iter();
-
-    RgbImage::from_fn(img1.width(), img1.height(), |_, _| {
-        let a = match img1_iter.next() {
-            Some(a) => a,
-            None => &Rgb([0,0,0]), 
-        };
-        let b = match img2_iter.next() {
-            Some(b) => b,
-            None => &Rgb([0,0,0]), 
-        };
-        Rgb([
-            a.0[0] ^ b.0[0],
-            a.0[1] ^ b.0[1],
-            a.0[2] ^ b.0[2],
+    let mut xored = img1.pixels()
+        .into_iter()
+        .zip(img2.pixels().into_iter())
+        .map(|(a, b)| {
+            Rgb([
+                a.0[0] ^ b.0[0],
+                a.0[1] ^ b.0[1],
+                a.0[2] ^ b.0[2],
             ])
+        });
+    RgbImage::from_fn(img1.width(), img1.height(), |_, _| {
+        match xored.next() {
+            Some(a) => a,
+            None => Rgb([0,0,0]), 
+        }
     })
 }
 
