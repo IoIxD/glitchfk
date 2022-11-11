10f33eff98712f3157bfc9b8f8c44e92f507b951
IoIxD <alphaproject217@gmail.com>
2022-11-10 18:22:01 -0700

    canned the web side of things in rust; will do that in golang along with all the other stuff.

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,7 @@
 pub mod modules;
 pub mod debug;
 pub mod image;
-pub mod services;
 
-// Compiling for server or anything else.
-#[cfg(not(target_arch = "wasm32"))]
-use services::server::server;
-
-#[cfg(not(target_arch = "wasm32"))]
-#[tokio::main]
-async fn main() {
-    
-    tokio::select! {
-        done = server::web_thread() => println!("{}",done.unwrap()),
-    };
-}
-
-// Compiling for WASM.
-#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
-use services::wasm::wasm;
-
-#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
 fn main() {
-    wasm::main();
+    
 }
