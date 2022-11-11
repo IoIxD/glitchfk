670ff0b49527407f30d50750182b08037b592a32
IoIxD <alphaproject217@gmail.com>
2022-11-10 18:55:32 -0700

    command line args

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,59 @@ pub mod modules;
 pub mod debug;
 pub mod image;
 
+use clap::{command, Command, Arg, ArgAction, arg};
+
 fn main() {
-    
+    let mut matches = command!()
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("images")
+                .about("xor two provided images together")
+                .arg(arg!(--width <VALUE>).required(false).action(ArgAction::Set))
+                .arg(arg!(--height <VALUE>).required(false).action(ArgAction::Set))
+                .arg(Arg::new("paths").required(true).action(ArgAction::Append))
+        )
+        .subcommand(
+            Command::new("generate")
+                .about("generate two gradients, randomly or from a prompt, and xor them together")
+                .arg(arg!(--width <VALUE>).required(false).action(ArgAction::Set))
+                .arg(arg!(--height <VALUE>).required(false).action(ArgAction::Set))
+                .arg(arg!(--types <VALUE>).required(false).action(ArgAction::Append))
+        );
+
+    let matches_list = matches.clone().get_matches();
+
+    match matches_list.subcommand() {
+        Some(("images", sub_matches)) => {
+            let paths = sub_matches
+            .get_many::<String>("path")
+            .unwrap_or_default()
+            .map(|v| v.as_str())
+            .collect::<Vec<_>>();
+        }
+        Some(("types", sub_matches)) => {
+            let paths = sub_matches
+            .get_many::<String>("paths")
+            .unwrap_or_default()
+            .map(|v| v.as_str())
+            .collect::<Vec<_>>();
+
+            if paths.len() <= 0 {
+                println!("Usage: glitchfuck generate [type1] [type2] [type3] ...")
+            } else {
+                gen_image(paths)
+            }
+        }
+        _ => {_ = matches.print_help();}
+    };
+}
+
+fn gen_image(paths: Vec<&str>) {
+
+}
+
+fn gen_gradient(types: Vec<&str>) {
+
 }
